211af2c79f71b1efd31845be69fa38180155857b
gryphius <oli@fuglu.org>
2014-10-29 13:43:11 +0100

    suppress InsecureRequestWarning, workaround for upstream #4

--- dwhois/query.py
+++ dwhois/query.py
@@ -16,6 +16,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 import requests
+requests.packages.urllib3.disable_warnings()
 import sys
 import urllib
 import urlparse
